df2b6404e23c3918c36eb72f64dbbb6f4ce86f52
dlosowyj <djl256@cornell.edu>
2017-04-21 11:01:35 -0500

    Added function to ex33.py that allows variable incrementing of the loop.

--- ex33.py
+++ ex33.py
@@ -8,12 +8,22 @@ def append_inc_print(ii, nums):
 
     return ii, nums
 
+def append_inc_print_2(ii, nums, inc):
+    print("At the top i is %d" %ii)
+    nums.append(ii)
+
+    ii = ii + inc
+    print("Numbers now: ", nums)
+    print("Ath the bottom i is %d" %ii)
+
+    return ii, nums
+
 i = 0
 numbers = []
 is_small = True
 
 while is_small:
-    i, numbers = append_inc_print(i, numbers)
+    i, numbers = append_inc_print_2(i, numbers, 2)
     
     if i > 5:
         is_small = False

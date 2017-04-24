a5c3fa03e996542ca1c2db6e4f4c3898c2d584cc
dlosowyj <djl256@cornell.edu>
2017-04-24 16:25:52 -0500

    Added genre and print_genre() to ex40.py.

---
+++ ex40.py
@@ -0,0 +1,25 @@
+class Song(object):
+
+    def __init__(self, lyrics, genre):
+        self.lyrics = lyrics
+        self.genre = genre
+
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print(line) 
+
+    def print_genre(self):
+        print(self.genre)
+
+happy_bday = Song(["Happy birthday to you",
+    "I don't want to get sued",
+    "So I'll stop right there"], "Death Metal")
+
+bulls_on_parade = Song(["They rally around tha family",
+    "With pockets full of shells"], "Classical")
+
+happy_bday.sing_me_a_song()
+happy_bday.print_genre()
+
+bulls_on_parade.sing_me_a_song()
+bulls_on_parade.print_genre()
